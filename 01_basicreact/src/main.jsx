e91fa6ca1013f9873b2558232feeb29722eb0016
Abhishek Yadav <abhishekyadav44121@gmail.com>
2026-06-19 01:05:17 +0530

    Add manifest.json to public folder and link in index.html

--- 01_basicreact/src/main.jsx
+++ 01_basicreact/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 
-import App from './App.jsx'
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);

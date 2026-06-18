10d033a879da7d917b68c9bbbad800c8d905ffc8
Abhishek Yadav <abhishekyadav44121@gmail.com>
2026-06-19 00:03:23 +0530

    Replace 01_basicreact with complete Vite + React project

---
+++ 01_basicreact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1>Hello everyone</h1>
+      <h2>I'm Abhishek a software developer</h2>
+    </div>
+  )
+}
+
+export default App
